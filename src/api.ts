231dc68e36c780907966faac9a06e7050b96040a
indusy <teririkawaii@outlook.com>
2022-03-17 19:30:42 +0800

    第二次重构

--- src/api.ts
+++ src/api.ts
@@ -1,57 +1,126 @@
-import axios from "axios";
+interface fetchOptions {
+  query?: apiQuery;
+  body?: Object;
+  path?: apiPath;
+}
 
-axios.defaults.baseURL = "https://bot.k2t3k.tk/api"
+enum httpMethods {
+  get = "GET",
+  post = "POST",
+  patch = "PATCH",
+  delete = "DELETE",
+}
 
-class Api {
+interface apiPath {
+  signature?: string;
+}
+interface apiQuery {
+  q?: string;
+  regex?: string;
+  per?: string;
+  page?: string;
+  appId?: string;
+}
+
+interface apiBody {}
+
+class API {
+  baseurl = "https://bot.k2t3k.tk/api";
   url: string;
-  usparams: string[];
-  pathparams: string[];
-  constructor(url: string, usparams: string[], pathparams: string[]) {
+  opts: fetchOptions;
+
+  constructor(url: string, opts: fetchOptions) {
     this.url = url;
-    this.usparams = usparams;
-    this.pathparams = pathparams;
+    this.opts = opts;
   }
-}
-
-class ApiUtil extends Api {
-  constructor(url: string, usparams: string[], pathparams: string[]) {
-    super(url, usparams, pathparams);
-  }
-  async get(usparams?: string[], pathparams?: string[]) {
-
-    let searchParam = new URLSearchParams();
-    let pathParam = ''
-
-    if(usparams != null) {
-        usparams.forEach((p, i) => {
-            searchParam.append(this.usparams[i],p);
-        });
-    }
-
-    if(pathparams != null) {
-        for(let i = 0; i < this.pathparams.length; i++) {
-            pathParam += `/${pathparams[i]}`;
-        }
-    }
-
-    return await axios({
-        method: "GET",
-        url: `${this.url}${pathParam}?${searchParam}`,
+  async request(opts: fetchOptions, method = httpMethods.get) {
+    let { query, body, path } = opts;
+    let url = this.baseurl + this.url;
+    let code: number;
+    if (this.opts.path) {
+      const pathRegex = /\{(.*?)}/;
+      url.match(pathRegex)?.forEach((k) => {
+        url = url.replace(`{${k}}`, (path as apiPath)[k as keyof typeof path]);
       });
+    }
+
+    if (this.opts.query) {
+      let usp = new URLSearchParams();
+      let uspStr: string;
+      let defaultQuery = this.opts.query as apiQuery;
+      let realQuery: string = "?";
+      let iterator = Object.entries(defaultQuery);
+      iterator.forEach((kv) => {
+        usp.append(kv[0], kv[1]);
+      });
+      if (query as apiQuery) {
+        query = query as apiQuery;
+        let iterator = Object.entries(query);
+        iterator.forEach((kv) => {
+          if (usp.has(kv[0])) {
+            usp.set(kv[0], kv[1]);
+          } else {
+            usp.append(kv[0], kv[1]);
+          }
+        });
+      }
+      uspStr = usp.toString();
+      realQuery = uspStr === "" ? uspStr : realQuery + uspStr;
+      url += realQuery;
+    }
+
+    return await fetch(url, {
+      method,
+      mode: 'cors',
+    })
   }
 }
 
-let searchAPI = new ApiUtil("/search", ['q', 'page', 'per'], []);
-let regexAPI = new ApiUtil("/search/regex", ['q', 'page', 'per'], []);
-let allAPI = new ApiUtil("/getAll", ['page', 'per'], []);
-let signatureAPI = new ApiUtil("/getAll", ['page', 'per'], ['signature']);
-let iconAPI = new ApiUtil("/icon", ['appId'], []);
+let appInfoApi = new API("/appInfo", {
+  query: {
+    q: "app-tracker",
+    per: "2147483647",
+    page: "1",
+  },
+});
+
+let getallApi = new API("/appInfo", {
+  query: {
+    per: "2147483647",
+    page: "1",
+  },
+});
+
+let regexApi = new API("/appInfo", {
+  query: {
+    regex: "app-tracker",
+    per: "2147483647",
+    page: "1",
+  },
+});
+
+// 尚未开放关键词搜索
+let signatureAppInfoApi = new API("/{signature}/appInfo", {
+  query: {
+    per: "2147483647",
+    page: "1",
+  },
+  path: {
+    signature: "app-tracker",
+  },
+});
+
+let appIconApi = new API("/icon", {
+  query: {
+    appId: "",
+  },
+});
 
 export {
-    ApiUtil,
-    searchAPI,
-    regexAPI,
-    allAPI,
-    signatureAPI,
-    iconAPI
+  appInfoApi,
+  getallApi,
+  regexApi,
+  appIconApi,
+  signatureAppInfoApi,
+  httpMethods,
 };
