44d4588fa8e7be4ac8f37978ef20670e5f2c1b3b
EndureBlaze <nihaocun@163.com>
2022-03-19 10:29:44 +0800

    fix icons

--- vite.config.ts
+++ vite.config.ts
@@ -16,10 +16,21 @@ export default defineConfig({
         theme_color: "#504ebc",
         icons: [
           {
-            src: "favicon.png",
-            type: "image/png",
-            sizes: "192x192",
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
         ],
         start_url: "/",
         background_color: "#504ebc",
