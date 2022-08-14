2e59943b0556b9278c6fc37038f6bcccd10c5687
吴峥 <teririkawaii@outlook.com>
2022-08-14 17:01:23 +0800

    fix: 生产环境获取应用名和版本

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = (window as any).__APP_ENV__.name;
-export const APP_VERSION = (window as any).__APP_ENV__.version;
+export const APP_NAME = import.meta.env.APP_NAME;
+export const APP_VERSION = import.meta.env.APP_VERSION;
 export const APP_ENV = import.meta.env.MODE;
 export const API_URL = import.meta.env.VITE_APP_API_URL;
